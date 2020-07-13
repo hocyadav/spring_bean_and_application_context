51f288ba7579bc292d6cd700c47af3536b59c8a5
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-13 19:42:52 +0530

    bean factory and application context

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,38 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
 import com.service.ServiceClass;
 
 public class Test {
 	public static void main(String[] args) {
-		ServiceClass service = new ServiceClass();
+		//m1 - working
+		ServiceClass service = new ServiceClass();//old way
 		service.fun();
+		
+		beanFactory__XmlBeanFactory();//m2 - working - method is deprecated but still we can get bean
+		beanFactory__ClassPathXmlApplicationContext();
+		applicationContext__ClassPathXmlApplicationContext();
 	}
+
+	private static void beanFactory__XmlBeanFactory() {
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring.xml"));//returns bean factory
+		ServiceClass bean = beanFactory.getBean("service1", ServiceClass.class);
+		bean.fun();
+	}
+	
+	private static void beanFactory__ClassPathXmlApplicationContext() {
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml");//ClassPathXmlApplicationContext return application context
+		ServiceClass bean = beanFactory.getBean("service1", ServiceClass.class);
+		bean.fun();
+	}
+	
+	private static void applicationContext__ClassPathXmlApplicationContext() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		ServiceClass bean = applicationContext.getBean("service1", ServiceClass.class);
+		bean.fun();
+	}
+
 }
