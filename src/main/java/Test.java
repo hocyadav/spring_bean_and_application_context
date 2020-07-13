c683cacb21232613295d4c96f0c5f46ff59dc08d
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-13 20:24:31 +0530

    update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 import com.service.ServiceClass;
@@ -15,6 +16,7 @@ public class Test {
 		beanFactory__XmlBeanFactory();//m2 - working - method is deprecated but still we can get bean
 		beanFactory__ClassPathXmlApplicationContext();
 		applicationContext__ClassPathXmlApplicationContext();
+		applicationContext__FileSystemXmlApplicationContext();
 	}
 
 	private static void beanFactory__XmlBeanFactory() {
@@ -30,7 +32,13 @@ public class Test {
 	}
 	
 	private static void applicationContext__ClassPathXmlApplicationContext() {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");//we can pass multiple xml file in argument
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");//make sure xml is in classpath:--> thats y method name is classpath
+		ServiceClass bean = applicationContext.getBean("service1", ServiceClass.class);
+		bean.fun();
+	}
+	private static void applicationContext__FileSystemXmlApplicationContext() {//put xml file anywhere
+		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("C:\\Users\\admin\\Documents\\workspace-sts-3.9.13.RELEASE\\spring_bean_n_appl_context\\src\\main\\resources\\spring.xml");
 		ServiceClass bean = applicationContext.getBean("service1", ServiceClass.class);
 		bean.fun();
 	}
